7a2f73c1e2e3106e4c0b1446371841a3ac6c28d0
gitcoder-23 <prabhatece21@gmail.com>
2025-11-09 19:28:32 +0530

    naigation package added

--- android/app/src/main/java/com/awesomeproject/MainActivity.kt
+++ android/app/src/main/java/com/awesomeproject/MainActivity.kt
@@ -5,6 +5,11 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
+// For react-native-screens
+import android.os.Bundle
+import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory
+
+
 class MainActivity : ReactActivity() {
 
   /**
@@ -19,4 +24,12 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+  
+    /**
+    * react-native-screens package requires one additional configuration step to properly work on Android devices
+    */
+  override fun onCreate(savedInstanceState: Bundle?) {
+      supportFragmentManager.fragmentFactory = RNScreensFragmentFactory()
+      super.onCreate(savedInstanceState)
+    }
 }
